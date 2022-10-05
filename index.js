4c09d218ffa38be9671f2a140466dfbcd5a7e145
Mensur Islami <mi@live.de>
2022-10-05 12:55:02 +0200

    Shtova një tregues shkarkimi

--- index.js
+++ index.js
@@ -1,36 +1,52 @@
 
 const getFjalet = async () => {
-    const response = await fetch("/fjalet.json")
+    const response = await fetch("https://raw.githubusercontent.com/mensaco/FjalorthIFjaleveTeHuaja/master/fjalet.json")
     const fjalet = response.json()
     return fjalet
 }
 let fjalet = {};
 
- getFjalet()
- .then(data => {
-        fjalet = data;
- })
- .catch(x => {
-    console.log(x)
-});
 
-const toSearch = (x) => x.replace(/ /g,'').toLowerCase().replace(/ë/gi,"e").replace(/ç/gi,"c") 
+[...document.querySelectorAll(".initially-shown")].forEach(x => x.classList.remove("hidden"));
+[...document.querySelectorAll(".initially-hidden")].forEach(x => x.classList.add("hidden"));
+
+getFjalet()
+    .then(data => {
+        fjalet = data;
+    })
+    .catch(x => {
+        console.log(x);
+    }).finally(() => {
+        [...document.querySelectorAll(".initially-shown")].forEach(x => x.classList.add("hidden"));
+        [...document.querySelectorAll(".initially-hidden")].forEach(x => x.classList.remove("hidden"));
+        document.querySelector(".out").classList.add('hidden');
+    })
+    ;
+
+
+
+
+const toSearch = (x) => {
+    return x.replace(/ /g, '').toLowerCase().replace(/ë/gi, "e").replace(/ç/gi, "c")
+}
 
 var onkeyup = (e) => {
-    const cv = toSearch(e.target.value)
-    if(!cv) {
+
+    const ev = e.target.value
+    if (!ev) {
         document.querySelector(".out").classList.add('hidden');
         document.querySelector("ul.out").innerHTML = '';
         return;
     };
+    const cv = toSearch(ev)
     const filt = Object.keys(fjalet).filter(f => toSearch(f).includes(cv))
     const afilt = filt.sort().map(f => "<li>" + f + " = " + fjalet[f] + "</li>")
     document.querySelector("ul.out").innerHTML = afilt.join('');
-    if(afilt.length > 0){
+    if (afilt.length > 0) {
         document.querySelector(".out").classList.remove('hidden');
     }
     else {
         document.querySelector(".out").classList.add('hidden');
     }
-    
+
 }
