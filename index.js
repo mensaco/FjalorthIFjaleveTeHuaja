f9c44fd027f8a53a4606f5d9430dd121edc3bedb
Mensur Islami <mi@live.de>
2022-10-06 00:03:05 +0200

    minor

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const nocache = new Headers();
-// nocache.append('pragma', 'no-cache');
-// nocache.append('cache-control', 'no-cache');
 
 var nocacheheaders = {
-  method: 'GET',
-  headers: nocache,
-  cache: 'reload'
+    method: 'GET',
+    headers: nocache,
+    cache: 'reload'
 };
 
 const getFjalet = async () => {
-    const response = await fetch(`https://raw.githubusercontent.com/mensaco/FjalorthIFjaleveTeHuaja/master/fjalet.json?r=${Math.random()}`, nocacheheaders)
+    const response = await fetch(`https://raw.githubusercontent.com/mensaco/FjalorthIFjaleveTeHuaja/master/fjalet.json`, nocacheheaders)
     const fjalet = response.json()
     return fjalet
 }
@@ -41,16 +39,19 @@ const toSearch = (x) => {
 var onkeyup = (e) => {
 
     const ev = e.target.value
+
     if (!ev) {
         console.log("!ev");
         document.querySelector(".out").classList.add('hidden');
-        document.querySelector("#otpt").innerHTML ='';
+        document.querySelector("#otpt").innerHTML = '';
         return;
     };
+
     const cv = toSearch(ev)
-    const filt = Object.keys(fjalet).filter(f => toSearch(f).includes(cv))
+    const filt = Object.keys(fjalet).filter(f => toSearch(f +  ' ' + fjalet[f]).includes(cv))
     const afilt = filt.sort().map(f => "<li>" + f + " = " + fjalet[f] + "</li>")
     document.querySelector("#otpt").innerHTML = afilt.join('');
+    
     if (afilt.length > 0) {
         document.querySelector(".out").classList.remove('hidden');
     }
