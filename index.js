2bb9d0b8ebc24566c593f66dd776d06e73ce9647
Mensur Islami <mi@live.de>
2023-04-04 16:08:23 +0200

    permiresim i filtrimit dhe paraqitjes

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-﻿function uuidv4() {
-    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+﻿function toSearch(x) {
+    if (x == undefined) x = this
+    return x.toLowerCase().replace(/ë/gi, "e").replace(/ç/gi, "c")
+}
+
+String.prototype.toSearch = toSearch
+
+function uuidv4() {
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     );
-  }
+}
+
+const landau = "&nbsp;&nbsp;&#x227B;&nbsp;&nbsp;"
 
 // anonymized id
 var ua = localStorage.getItem("ua");
-if(!ua){
+if (!ua) {
     ua = uuidv4();
-    localStorage.setItem("ua",ua);
+    localStorage.setItem("ua", ua);
 }
 
-document.querySelector("#ua").src = "https://mensaco.org/ua/"+ua+"/" + new Date().toISOString();
+document.querySelector("#ua").src = "https://mensaco.org/ua/" + ua + "/" + new Date().toISOString();
 
 const nocache = new Headers();
 
@@ -23,15 +32,13 @@ var nocacheheaders = {
 };
 
 const getFjalet = async () => {
-    const response = await fetch("https://raw.githubusercontent.com/mensaco/FjalorthIFjaleveTeHuaja/master/fjalet.json?v=04214da5-23d9-4fbb-8538-9234e9924896", nocacheheaders)
-    //const response = await fetch(`/fjalet.json`, nocacheheaders)
+    const response = await fetch("https://raw.githubusercontent.com/mensaco/FjalorthIFjaleveTeHuaja/master/fjalet.json?v=eadee7bb-af44-4c5e-a39b-cd82aa46c288", nocacheheaders)
     const fjalet = response.json()
     return fjalet
 }
 
 const getAutoret = async () => {
-    const response = await fetch("https://raw.githubusercontent.com/mensaco/FjalorthIFjaleveTeHuaja/master/autoret.json?v=04214da5-23d9-4fbb-8538-9234e9924896", nocacheheaders)
-    //const response = await fetch(`/autoret.json`, nocacheheaders)
+    const response = await fetch("https://raw.githubusercontent.com/mensaco/FjalorthIFjaleveTeHuaja/master/autoret.json?v=eadee7bb-af44-4c5e-a39b-cd82aa46c288", nocacheheaders)
     const autoret = response.json()
     return autoret
 }
@@ -39,6 +46,7 @@ const getAutoret = async () => {
 
 let fjalet = [];
 let autoret = [];
+let items = [];
 
 
 [...document.querySelectorAll(".initially-shown")].forEach(x => x.classList.remove("hidden"));
@@ -49,8 +57,6 @@ document.querySelector("#drbtn").addEventListener('click', drawAllItems);
 getAutoret()
     .then(data => {
         autoret = data;
-        //console.log(data)
-        //drawAllItems();
     })
     .catch(x => {
         //todo: error modal
@@ -61,8 +67,9 @@ getAutoret()
 getFjalet()
     .then(data => {
         const sortedKeys = Object.keys(data).sort();
-        fjalet = sortedKeys.map(sk => [sk, data[sk][0], autoret[data[sk][1]], true] );      
-        drawAllItems();  
+        fjalet = sortedKeys.map(sk => [sk, data[sk][0], autoret[data[sk][1]], true]);
+        items = fjalet.map(f => new DisplayItem(f))
+        drawAllItems();
     })
     .catch(x => {
         //todo: error modal
@@ -71,45 +78,53 @@ getFjalet()
     });
 
 
+class DisplayItem {
+    constructor(f) {
+        this.Huaj = f[0]
+        this.Shqip = f[1]
+        this.Autori = f[2] ? ` title="${f[2]}"`: ""
+        
+        this.filter = true
 
+        this.SetFilter = (q) => {
+            this.Q = q
+            this.r = this.Q.toSearch()
+            this.posHuaj = this.Huaj.toSearch().indexOf(this.r)
+            this.posShqip = this.Shqip.toSearch().indexOf(this.r)
+            this.filter = this.posHuaj > -1 || this.posShqip > -1
+            this.FormattedHuaj = `<span class="strikediag">${this.Huaj}</span>`
+            this.FormattedShqip = this.Shqip
+            if(this.posHuaj>-1){
+                this.PartsHuaj =  [this.Huaj.substring(0, this.posHuaj), this.Huaj.substring(this.posHuaj, this.posHuaj + this.Q.length), this.Huaj.substring(this.posHuaj + this.Q.length)]
+                this.FormattedHuaj = `<span class="strikediag">${this.PartsHuaj[0]}<span class="text-orange-500">${this.PartsHuaj[1]}</span>${this.PartsHuaj[2]}</span>`
+            }
+            if(this.posShqip > -1){
+                this.PartsShqip =  [this.Shqip.substring(0, this.posShqip), this.Shqip.substring(this.posShqip, this.posShqip + this.Q.length), this.Shqip.substring(this.posShqip + this.Q.length)]            
+                this.FormattedShqip = `${this.PartsShqip[0]}<span class="text-orange-500">${this.PartsShqip[1]}</span>${this.PartsShqip[2]}`
+            }
+            
+        }
 
+    }
 
-
-
-const toSearch = (x) => {
-    return x.replace(/ /g, '').toLowerCase().replace(/ë/gi, "e").replace(/ç/gi, "c")
-}
-
-function toformatted(f,cv) {
-    const title = f[2] != "" ?  ` title="${f[2]}"` : ""
-    const fs1 = f[0].replace(cv, `<span class="text-orange-500">${cv}</span>`)
-    const fs2 = f[1].replace(cv, `<span class="text-orange-500">${cv}</span>`)
-    const landau = "&nbsp;&nbsp;&#x227B;&nbsp;&nbsp;"
-
-    return `<li${title}>
-                <span class="strikediag">${fs1}</span>${landau}${fs2}
-            </li>`
 }
 
 function drawItems(e) {
     var ev = ""
     var cv = ""
-    if(e){
-         ev = e.target.value
-         cv = toSearch(ev)
+    if (e) {
+        ev = e.target.value
+        cv = toSearch(ev)
     }
 
-    const f1 = fjalet.filter(f => (f[0] + ' ' + f[1]).includes(cv))
-    //f1.forEach(f => f[2] = f[2] != autoret[0] ? "botuesi " + f[2] : "" )
+    var filteredItems = items
+    filteredItems.forEach(i => i.SetFilter(ev))
 
-    // const afilt = fjalet.map(f => 
-    //     (f[0] + ' ' + f[1]).includes(cv) 
-    //     ? "<li title='botuesi "+f[2]+"'>" + "<span class='strikediag' >" + f[0].replace(cv, "<span class='text-orange-500' >"+cv+"</span>") + "</span>" + "&nbsp;&nbsp;&#x227B;&nbsp;&nbsp;" + f[1].replace(cv, "<span class='text-orange-500' >"+cv+"</span>") + "</li>" 
-    //     : "")
-
-    const afilt = f1.map(f => toformatted(f, cv))
-
-    document.querySelector("#otpt").innerHTML = afilt.join('');
+    filteredItems = filteredItems.filter(fi => fi.filter)
+   
+    document.querySelector("#otpt").innerHTML = filteredItems.map(fi => `<li${fi.Autori}>
+    ${fi.FormattedHuaj}${landau}${fi.FormattedShqip}
+</li>`).join('');
 
 
 }
